219ceb40fc317a53b475c594f8d1269fa5d55555
ianoxwell <ianoxwell@gmail.com>
2025-05-23 17:05:25 +1000

    feat: initial api seeding

---
+++ api.quickbeans/src/Controllers/venue/Venue.entity.ts
@@ -0,0 +1,52 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Venue {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  websiteUrl: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ nullable: true })
+  logoImage: string;
+
+  @Column()
+  countryId: string;
+
+  @Column({ type: 'jsonb' })
+  openingHours: { day: string; open: string; close: string }[];
+
+  @Column()
+  address: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  postcode: string;
+
+  @Column()
+  publicPhone: string;
+
+  @Column()
+  legalBusinessName: string;
+
+  @Column()
+  legalBusinessNumber: string;
+
+  @Column()
+  timezone: string;
+
+  @Column()
+  privacyPolicy: string;
+}
