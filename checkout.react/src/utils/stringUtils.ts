6b86cd0d22638f03f5180fae7aabcb39a16e61b5
ianoxwell <ianoxwell@gmail.com>
2025-05-28 14:19:31 +1000

    feat: some basic utils to start with

---
+++ checkout.react/src/utils/stringUtils.ts
@@ -0,0 +1,16 @@
+export const sentenceCase = (sentence: string | undefined): string => {
+  if (!sentence) {
+    return '';
+  }
+
+  return sentence[0].toUpperCase() + sentence.substring(1).toLowerCase();
+};
+
+export const parseRecipeInstructions = (htmlString: string) => {
+  // Create a temporary DOM element to parse the HTML
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, 'text/html');
+
+  // Extract text content from <li> elements inside <ol>
+  return Array.from(doc.querySelectorAll('ol li')).map((li) => li.textContent?.trim());
+};
