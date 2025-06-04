997c0d028d69d8e198521927ad96c3140a78cf9a
ianoxwell <ianoxwell@gmail.com>
2025-06-04 11:02:22 +1000

    feat: ability to set size on quantity input

--- checkout.react/src/components/QuantityInput/QuantityInput.component.tsx
+++ checkout.react/src/components/QuantityInput/QuantityInput.component.tsx
@@ -1,16 +1,24 @@
-import { ActionIcon, Flex, NumberInput, NumberInputHandlers, Tooltip } from '@mantine/core';
+import { ActionIcon, NumberInput, NumberInputHandlers, Tooltip } from '@mantine/core';
 import { Minus, Plus } from 'lucide-react';
 import { useRef } from 'react';
 import './QuantityInput.component.scss';
 
-const QuantityInput = ({ quantity, onChange }: { quantity: number; onChange: (value: number | string) => void }) => {
+const QuantityInput = ({
+  quantity,
+  size,
+  onChange
+}: {
+  quantity: number;
+  size: 'sm' | 'lg';
+  onChange: (value: number | string) => void;
+}) => {
   const handlersRef = useRef<NumberInputHandlers>(null);
   const iconSize = 16;
 
   return (
-    <Flex gap="xs" className="quantity-input" align="center">
+    <div className={`quantity-input size--${size}`}>
       <ActionIcon
-        size="input-sm"
+        size={size === 'sm' ? 'input-xs' : 'input-sm'}
         onClick={() => handlersRef.current?.decrement()}
         type="button"
         radius="xl"
@@ -28,11 +36,12 @@ const QuantityInput = ({ quantity, onChange }: { quantity: number; onChange: (va
           max={99}
           value={quantity}
           onChange={onChange}
+          size={size === 'sm' ? 'xs' : 'md'}
           hideControls
         />
       </Tooltip>
       <ActionIcon
-        size="input-sm"
+        size={size === 'sm' ? 'input-xs' : 'input-sm'}
         onClick={() => handlersRef.current?.increment()}
         type="button"
         radius="xl"
@@ -40,7 +49,7 @@ const QuantityInput = ({ quantity, onChange }: { quantity: number; onChange: (va
       >
         <Plus size={iconSize} />
       </ActionIcon>
-    </Flex>
+    </div>
   );
 };
 
