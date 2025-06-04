f47d94c5468753ac5703abf256476cb5cc40ee3c
ianoxwell <ianoxwell@gmail.com>
2025-06-04 16:46:26 +1000

    feat: fake payment and confirmation page holder

---
+++ checkout.react/src/pages/confirmation/ConfirmationPage.tsx
@@ -0,0 +1,12 @@
+const ConfirmationPage = () => {
+  return (
+    <div>
+      <h1>Confirmation page</h1>
+      <p>Thank you for your order!</p>
+      <p>Your order has been successfully placed.</p>
+      <div>Listen to the other status on web</div>
+    </div>
+  );
+};
+
+export default ConfirmationPage;
---
+++ checkout.react/src/pages/payment/PaymentPage.tsx
@@ -0,0 +1,9 @@
+const PaymentPage = () => {
+  return (
+    <div>
+      <h1>Fake Payment page</h1>
+    </div>
+  );
+};
+
+export default PaymentPage;
